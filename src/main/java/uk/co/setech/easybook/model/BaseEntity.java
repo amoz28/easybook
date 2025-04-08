277c53e3a781f45152f7d8886cd7bbd01d1ac900
Amos Okomayin <124576225+amoz28@users.noreply.github.com>
2025-04-08 22:31:01 +0100

    Updated the id generation strategy

--- src/main/java/uk/co/setech/easybook/model/BaseEntity.java
+++ src/main/java/uk/co/setech/easybook/model/BaseEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @CreationTimestamp
     private LocalDateTime createdAt;
